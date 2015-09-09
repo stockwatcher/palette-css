c14d27cf6091ed80d663f971cb15725ff38644ac
Jason B. Cox <contact@jasonbcox.com>
2015-09-09 01:01:09 -0700

    Initial Commit

---
+++ render_index.js
@@ -0,0 +1,63 @@
+
+var Content = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Color Palette
+        <div className="row">
+          <div className="col s2 green darken-2">green darken-2</div>
+          <div className="col s2 light-green">light-green</div>
+          <div className="col s2 yellow">yellow</div>
+          <div className="col s2 red">red darken-1</div>
+          <div className="col s2 red darken-3">red darken-3</div>
+        </div>
+
+        Grid System
+        <div className="row">
+          <div className="col s1 red">1</div>
+          <div className="col s2 orange">2</div>
+          <div className="col s3 yellow">3</div>
+          <div className="col s3 green">4</div>
+          <div className="col s2 blue">5</div>
+          <div className="col s1 purple">6</div>
+        </div>
+
+        Grid-Based Table of Updates
+        <div>
+          <div className="row">
+            <div className="col offset-s3 s6 striped-1">
+              <div className="update">
+                <div className="avatar-image pull-left indigo"></div>
+                <div className="update-content">
+                  <div className="update-header">
+                    <div className="left">Example Left Header</div>
+                    <div className="right">Example Right Header</div>
+                  </div>
+                  <div>Example content with words and lots of cool text.</div>
+                </div>
+              </div>
+              <div className="update">
+                <div className="avatar-image pull-left indigo"></div>
+                <div className="update-content">
+                  <div>This update shows off the striped-n mixin.</div>
+                </div>
+              </div>
+              <div className="update">
+                <div className="avatar-image pull-left indigo"></div>
+                <div className="update-content"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+React.render(
+  <div>
+    <Content />
+  </div>,
+  document.getElementById('container')
+);
+
